3e8d9da2886a4d1cf5fe44c2b75420f7ccaf14fd
Mahaoui Abdennasser <7na2ro@gmail.com>
2020-04-24 16:26:31 +0100

    LIBRARY JS PROGECT

--- js/LIBRARY/js/javascript.js
+++ js/LIBRARY/js/javascript.js
@@ -3,15 +3,21 @@
 
  let 
     save = document.querySelector('button'),
+    trash = document.getElementById('delete'),
     // reset = document.getElementById('reset'),
     bookTitle = document.getElementById('bookTitle'),
     bookAuteur = document.getElementById('bookAuteur'),
     bookPages = document.getElementById('bookPages');
 
 
- 
-    save.onclick =  function (){
+    trash.onclick = function (){
+        // trtr = document.getElementById('title');
+        rem = trash.parentElement.parentElement;
+        rem.remove(rem);
         
+    }
+    
+    save.onclick =  function (){
             let title     =  document.getElementById('title').value,
                 auteur    =  document.getElementById('auteur').value,
                 pages     =  document.getElementById('pages').value,
@@ -27,13 +33,14 @@
             main.appendChild(newBook);
             newBook.appendChild(newTitle).innerHTML  = title;
             newBook.appendChild(newAuteur).innerHTML = auteur;
-            newBook.appendChild(newPages).innerHTML  = pages;
+            newBook.appendChild(newPages).outerHTML  = `<h4 class="page">${pages}</h4>`;
+            // newBook.appendChild(newPages).innerHTML  = pages;
             if (readIt.checked) {
                 newBook.appendChild(newEye).outerHTML = '<i class="far fa-eye fa-lg" style="color: brown;align-self: center; padding: 0 10px; margin: 10px;"></i>';
             } else {
                 newBook.appendChild(newEye).outerHTML = '<i class="far fa-eye-slash" style="color: #ededed; align-self: center; padding: 0 10px; margin: 10px;"></i>';
             }
-            newBook.appendChild(newDelete).outerHTML = '<i class="fas fa-trash-alt fa-lg" style="color: red; align-self: center; padding: 0 10px; margin: 10px;"></i>'
+            newBook.appendChild(newDelete).outerHTML = '<span><button type="button" id="delete"><i class="fas fa-trash-alt fa-lg" style="color: red; align-self: center; padding: 0 10px; margin: -6px;"></i></button></span>'
                 // console.log(readIt.checked); align-self: center; padding: 0 10px; margin: 10px;
         }
     // function reset(){
